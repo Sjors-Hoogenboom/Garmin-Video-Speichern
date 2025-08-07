ac6e4ad13ae624959106aa5e67e2d5bb3740ca72
Sjors-Hoogenboom <SjorsH.2004@gmail.com>
2025-08-07 14:06:33 +0200

    Made some config into variables

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ import speech_recognition as sr
 
 recognizer = sr.Recognizer()
 
+# The original meme is German, change to en-US for English speech recognition
+LANGUAGE = "de-DE"
+
+# These are the words that the application will look for
+FIRST_TARGET_WORD = "Garmin"
+SECOND_TARGET_WORD = "Video speichern"
+
+# Files of the audio, have to be .wav
+FILENAME_ONE = "GarminListening.wav"
+FILENAME_TWO = "GarminConfirmed.wav"
+
 OUTPUT_DEVICE_INDEX = 9
 MICROPHONE_DEVICE_INDEX = 4
 p = pyaudio.PyAudio()
@@ -46,7 +57,7 @@ def listen_and_recognize(prompt, target_word):
         audio = recognizer.listen(source)
 
         try:
-            text = recognizer.recognize_google(audio, language="de-DE")
+            text = recognizer.recognize_google(audio, language=LANGUAGE)
             print("You said:", text)
             return is_word_close_enough(text, target_word)
         except sr.UnknownValueError:
@@ -57,18 +68,17 @@ def listen_and_recognize(prompt, target_word):
     return False
 
 with sr.Microphone(device_index=MICROPHONE_DEVICE_INDEX) as source:
-    print("Calibrating microphone for background noise...")
     recognizer.adjust_for_ambient_noise(source, duration=0.5)
 
 try:
     while True:
-        if listen_and_recognize("Say Garmin", "Garmin"):
-            play_sound("GarminListening.wav")
-            print("Garmin!")
-            if listen_and_recognize("Say video speichern", "Video speichern"):
-                print("Video speichern!")
+        if listen_and_recognize("\nSay Garmin", FIRST_TARGET_WORD):
+            play_sound(FILENAME_ONE)
+            print(FIRST_TARGET_WORD + "!")
+            if listen_and_recognize("\nSay video speichern", SECOND_TARGET_WORD):
+                print(SECOND_TARGET_WORD + "!")
                 pyautogui.hotkey('ctrl', ',')
-                play_sound("GarminConfirmed.wav")
+                play_sound(FILENAME_TWO)
             else:
                 print("Video not detected")
         else:
